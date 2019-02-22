78639fffa28036b3fc4440ffe38b35174b1d827d
PeroFuse <>
2019-02-22 11:35:33 +0200

    Added adminGroupManager bones

--- client/dms-react-app/src/containers/AdminBoardGroups.js
+++ client/dms-react-app/src/containers/AdminBoardGroups.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import AdminNavBar from '../components/AdminNavBar/AdminNavBar';
 import Pagening from '../components/Pagening/Pagening';
 import Footer from '../components/Footer/Footer';
+import AdminGroupManager from '../components/groupManager/AdminGroupManager';
 
 class AdminBoardGroups extends Component {
     render() {
         return (
             <div>
                 <AdminNavBar/> 
+                <AdminGroupManager/>
                 <Pagening/>
                 <Footer/> 
             </div>
