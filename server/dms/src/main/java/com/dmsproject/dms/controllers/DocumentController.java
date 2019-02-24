00ef787695eae57af4e23dbc927e8cddec325744
ina <i_samanaviciute@yahoo.com>
2019-02-25 00:44:31 +0200

    Pridetas document/get/all

--- server/dms/src/main/java/com/dmsproject/dms/controllers/DocumentController.java
+++ server/dms/src/main/java/com/dmsproject/dms/controllers/DocumentController.java
@@ -2,10 +2,14 @@ package com.dmsproject.dms.controllers;
 
 
 import com.dmsproject.dms.dao.DocumentDAO;
+import com.dmsproject.dms.dto.DocSelection;
 import com.dmsproject.dms.dto.Document;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 
 @RestController()
@@ -32,11 +36,22 @@ public class DocumentController {
 //    }
 
     @RequestMapping(value = "/document/add", method = RequestMethod.POST, consumes = "application/json")
-    public Boolean add(@RequestBody String docContent){
+    public Boolean add(@RequestBody String docContent) {
         Document document = new Document();
         document.setTypeId(1);
         document.setContent(docContent);
         return DocumentDAO.addDocument(document);
     }
+
+
+    @RequestMapping (value = "/document/get/all", method = RequestMethod.GET, produces = "application/json")
+    public List<DocSelection> getAll(HttpServletRequest httpServletRequest) {
+        return DocumentDAO.getAllDocuments();
+    }
+
+//    @RequestMapping (value = "/document/get/byId", method = RequestMethod.GET, produces = "application/json")
+//    public List<DocSelection> getAll(HttpServletRequest httpServletRequest) {
+//        return DocumentDAO.getAllDocuments();
+//    }
 
 }
--- server/dms/src/main/java/com/dmsproject/dms/dao/DocumentDAO.java
+++ server/dms/src/main/java/com/dmsproject/dms/dao/DocumentDAO.java
@@ -1,9 +1,13 @@
 package com.dmsproject.dms.dao;
 
 import com.dmsproject.dms.Database;
+import com.dmsproject.dms.dto.DocSelection;
 import com.dmsproject.dms.dto.Document;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DocumentDAO {
     private static final String INSERT_SQL = "INSERT INTO documents" +
@@ -15,7 +19,7 @@ public class DocumentDAO {
             PreparedStatement statement = Database.connection.prepareStatement(INSERT_SQL);
             statement.setInt(1, document.getTypeId());
             statement.setString(2, document.getName());
-            statement.setString( 3, document.getNumber());
+            statement.setString(3, document.getNumber());
             statement.setString(4, document.getContent());
 
             statement.executeUpdate();
@@ -27,4 +31,80 @@ public class DocumentDAO {
         }
         return true;
     }
+
+
+    public static ArrayList<DocSelection> getAllDocuments() {
+
+        String query = "SELECT documents.doc_number, users.name, users.surname, document_types.doc_type_descr, documents.doc_name, status.status_descr, document_status.date, document_status.doc_status_descr " +
+                "FROM document_status " +
+                "INNER JOIN documents ON document_status.document_id=documents.doc_id " +
+                "INNER JOIN users ON document_status.user_id=users.user_id " +
+                "INNER JOIN status ON document_status.status_id= status.status_id " +
+                "INNER JOIN document_types ON documents.doc_type_id=document_types.doc_type_id";
+
+        ArrayList documentsList = new ArrayList<DocSelection>();
+
+        try {
+            PreparedStatement statement = Database.connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                DocSelection docSelection = new DocSelection();
+
+                docSelection.setNumber(rs.getString("doc_number"));
+                docSelection.setUserName(rs.getString("name"));
+                docSelection.setUserSurname(rs.getString("surname"));
+                docSelection.setType(rs.getString("doc_type_descr"));
+                docSelection.setName(rs.getString("doc_name"));
+                docSelection.setStatus(rs.getString("status_descr"));
+                docSelection.setDate(rs.getString("date"));
+                docSelection.setStatusDescr(rs.getString("doc_status_descr"));
+
+
+                documentsList.add(docSelection);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return documentsList;
+    }
+
+
+    public static ArrayList<Document> searchByUser(int id) {
+
+
+        String query = "SELECT document_status.user_id, documents.doc_id, documents.doc_type_id, documents.doc_name, documents.doc_number, documents.doc_content " +
+                " FROM documents " +
+                " INNER JOIN document_status ON documents.doc_id=document_status.document_id " +
+                " WHERE document_status.user_id=1";
+
+        ArrayList documentsList = new ArrayList<Document>();
+
+        try {
+            PreparedStatement statement = Database.connection.prepareStatement(INSERT_SQL);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                Document document = new Document();
+                document.setId(rs.getInt("doc_id"));
+                document.setTypeId(rs.getInt("doc_type_id"));
+                document.setName(rs.getString("doc_name"));
+                document.setNumber(rs.getString("doc_number"));
+                document.setContent(rs.getString("doc_content"));
+
+                documentsList.add(document);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return documentsList;
+    }
+
+
 }
