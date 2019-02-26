0bbcb38e4f531d86fe87fb2819036d68c834c98c
ina <i_samanaviciute@yahoo.com>
2019-02-26 19:13:26 +0200

    Prideta documentTypes/get

--- server/dms/src/main/java/com/dmsproject/dms/dao/DocTypesDAO.java
+++ server/dms/src/main/java/com/dmsproject/dms/dao/DocTypesDAO.java
@@ -1,9 +1,13 @@
 package com.dmsproject.dms.dao;
 
 import com.dmsproject.dms.Database;
+import com.dmsproject.dms.dto.DocSelection;
 import com.dmsproject.dms.dto.DocTypes;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DocTypesDAO {
     private static final String INSERT_SQL = "INSERT INTO document_types" +
@@ -23,5 +27,34 @@ public class DocTypesDAO {
             return false;
         }
         return true;
+    }
+
+
+    public static ArrayList<DocTypes> getDocTypes() {
+
+        String query = "SELECT * FROM document_types";
+
+        ArrayList docTypesList = new ArrayList<DocTypes>();
+
+        try {
+            PreparedStatement statement = Database.connection.prepareStatement(query);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                DocTypes docTypes = new DocTypes();
+
+                docTypes.setId(rs.getInt("doc_type_Id"));
+                docTypes.setDescription(rs.getString("doc_type_descr"));
+
+
+                docTypesList.add(docTypes);
+            }
+
+            statement.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return docTypesList;
     }
 }
