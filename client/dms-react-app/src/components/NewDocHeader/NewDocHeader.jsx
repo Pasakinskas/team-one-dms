3f4468e564cdcf60cefce79335bd2eee3d27f9f2
anata <ana.tauriene@gmail.com>
2019-03-08 08:54:57 +0200

    added adresCard message onMouseOver

--- client/dms-react-app/src/components/NewDocHeader/NewDocHeader.jsx
+++ client/dms-react-app/src/components/NewDocHeader/NewDocHeader.jsx
@@ -8,16 +8,36 @@ import './NewDocHeader.css';
 library.add(faAddressCard)
 
 class NewDocHeader extends Component {
-
+  constructor() {
+    super();
+    this.state = { 
+      text : '',
+    }
+  }
     nextPath = (path)=>{
         this.props.history.push(path);
-      }
+    }
+
+    onMouseover = (e) => {
+      this.setState({
+        text : 'Grįžti į sąrašą',
+      })
+    }
+
+    //clear the text
+    onMouseout = (e) => {
+      this.setState({
+        text : '',
+      })
+    }
 
     render() {
+      const {text} = this.state;
         return (
           <div className="newDocHead">
-            <FontAwesomeIcon className="one" icon="address-card" onClick={() =>this.nextPath(`/userboard`)}/>
+            <FontAwesomeIcon className="one" icon="address-card" onClick={() =>this.nextPath(`/userboard`)}  onMouseOver = {this.onMouseover} onMouseOut = {this.onMouseout}/>
             <Button className="SignOut" variant="outline-info" onClick={() =>this.nextPath(`/login`)}>Atsijungti</Button>
+            <p>  {this.state.text}</p>
           </div> 
         );
     }
