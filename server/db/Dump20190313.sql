d8637c33b392a2210591a69692a528f45fe12b39
InaSG <45654490+InaSG@users.noreply.github.com>
2019-03-13 09:56:49 +0200

    Add files via upload
    changed 2019-03-13

---
+++ server/db/Dump20190313.sql
@@ -0,0 +1,385 @@
+-- MySQL dump 10.13  Distrib 8.0.13, for macos10.14 (x86_64)
+--
+-- Host: 127.0.0.1    Database: dvs
+-- ------------------------------------------------------
+-- Server version	8.0.15
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+ SET NAMES utf8 ;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `document_receiver`
+--
+
+DROP TABLE IF EXISTS `document_receiver`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `document_receiver` (
+  `doc_receiv_id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_id` int(11) NOT NULL,
+  `receiver_id` int(11) NOT NULL,
+  PRIMARY KEY (`doc_receiv_id`),
+  KEY `doc_id_idx` (`doc_id`),
+  KEY `doc-receiv_receiver_id_idx` (`receiver_id`),
+  CONSTRAINT `doc-receiv_receiver_id` FOREIGN KEY (`receiver_id`) REFERENCES `receivers` (`receiver_id`),
+  CONSTRAINT `doc_receiv_doc_id` FOREIGN KEY (`doc_id`) REFERENCES `documents` (`doc_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `document_receiver`
+--
+
+LOCK TABLES `document_receiver` WRITE;
+/*!40000 ALTER TABLE `document_receiver` DISABLE KEYS */;
+INSERT INTO `document_receiver` VALUES (3,3,1),(4,7,2),(5,9,5);
+/*!40000 ALTER TABLE `document_receiver` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `document_status`
+--
+
+DROP TABLE IF EXISTS `document_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `document_status` (
+  `doc_status_id` int(11) NOT NULL AUTO_INCREMENT,
+  `document_id` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `doc_status_descr` varchar(50) DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`doc_status_id`),
+  KEY `document_status_documents_FK_idx` (`document_id`),
+  KEY `document_status_status_FK_idx` (`status_id`),
+  KEY `document_status_user_FK_idx` (`user_id`),
+  CONSTRAINT `document_status_documents_FK` FOREIGN KEY (`document_id`) REFERENCES `documents` (`doc_id`),
+  CONSTRAINT `document_status_status_FK` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`),
+  CONSTRAINT `document_status_user_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `document_status`
+--
+
+LOCK TABLES `document_status` WRITE;
+/*!40000 ALTER TABLE `document_status` DISABLE KEYS */;
+INSERT INTO `document_status` VALUES (1,2,1,2,NULL,'2019-02-21 00:25:26'),(2,3,2,1,NULL,'2019-02-21 00:25:26'),(3,4,3,2,NULL,'2019-02-21 00:25:26'),(4,5,4,1,'bad mood','2019-02-21 00:25:26'),(5,6,1,2,NULL,'2019-02-21 00:25:26'),(6,7,2,1,NULL,'2019-02-21 00:25:26'),(7,8,3,2,NULL,'2019-02-21 00:25:26'),(9,9,3,1,NULL,'2019-03-13 00:05:34');
+/*!40000 ALTER TABLE `document_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `document_types`
+--
+
+DROP TABLE IF EXISTS `document_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `document_types` (
+  `doc_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_type_descr` varchar(45) NOT NULL,
+  `doc_template` longtext NOT NULL,
+  PRIMARY KEY (`doc_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `document_types`
+--
+
+LOCK TABLES `document_types` WRITE;
+/*!40000 ALTER TABLE `document_types` DISABLE KEYS */;
+INSERT INTO `document_types` VALUES (1,'Atostogų prašymas','fsffdf'),(2,'Prašymas atleisti iš darbo','sfasdf'),(3,'111111111','222222222');
+/*!40000 ALTER TABLE `document_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `documents`
+--
+
+DROP TABLE IF EXISTS `documents`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `documents` (
+  `doc_id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_type_id` int(11) DEFAULT NULL,
+  `doc_name` varchar(150) DEFAULT NULL,
+  `doc_number` varchar(50) DEFAULT NULL,
+  `doc_content` longtext NOT NULL,
+  PRIMARY KEY (`doc_id`),
+  KEY `documents_doc_type_FK_idx` (`doc_type_id`),
+  CONSTRAINT `documents_doc_type_FK` FOREIGN KEY (`doc_type_id`) REFERENCES `document_types` (`doc_type_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `documents`
+--
+
+LOCK TABLES `documents` WRITE;
+/*!40000 ALTER TABLE `documents` DISABLE KEYS */;
+INSERT INTO `documents` VALUES (2,1,'aaa','222','{\"json\":\"data1111\"}'),(3,1,'bbb','333','333bbb'),(4,1,'ccc','444','444ccc'),(5,2,'ddd','555','555ddd'),(6,2,'eee','666','666eee'),(7,2,'fff','777','777fff'),(8,2,'ggg','888','888ggg'),(9,1,'hhh','999','999hhh');
+/*!40000 ALTER TABLE `documents` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event_type`
+--
+
+DROP TABLE IF EXISTS `event_type`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `event_type` (
+  `event_type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `event_descr` varchar(45) NOT NULL,
+  PRIMARY KEY (`event_type_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `event_type`
+--
+
+LOCK TABLES `event_type` WRITE;
+/*!40000 ALTER TABLE `event_type` DISABLE KEYS */;
+/*!40000 ALTER TABLE `event_type` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `events` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `doc_id` int(11) DEFAULT NULL,
+  `event_type_id` int(11) DEFAULT NULL,
+  `event_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`event_id`),
+  KEY `events_doc_id_idx` (`doc_id`),
+  KEY `events_event_type_id_idx` (`event_type_id`),
+  KEY `events_user_id_idx` (`user_id`),
+  CONSTRAINT `events_doc_id` FOREIGN KEY (`doc_id`) REFERENCES `documents` (`doc_id`),
+  CONSTRAINT `events_event_type_id` FOREIGN KEY (`event_type_id`) REFERENCES `event_type` (`event_type_id`),
+  CONSTRAINT `events_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `events`
+--
+
+LOCK TABLES `events` WRITE;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+INSERT INTO `events` VALUES (2,2,NULL,'2019-03-12 11:35:58',1),(3,3,NULL,'2019-03-12 11:35:59',2),(4,4,NULL,'2019-03-12 11:35:59',1),(5,5,NULL,'2019-03-12 11:35:59',2),(6,6,NULL,'2019-03-12 11:35:59',2),(7,7,NULL,'2019-03-12 11:35:59',2),(8,8,NULL,'2019-03-12 11:35:59',1),(9,9,NULL,'2019-03-13 00:07:00',1);
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `group_permissions`
+--
+
+DROP TABLE IF EXISTS `group_permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `group_permissions` (
+  `group_perm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `permission_id` int(11) NOT NULL,
+  PRIMARY KEY (`group_perm_id`),
+  KEY `grpup_permissions_group_FK_idx` (`group_id`),
+  KEY `group_permissions_permission_id_idx` (`permission_id`),
+  CONSTRAINT `group_permissions_permission_id` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`permission_id`),
+  CONSTRAINT `grpup_permissions_group_FK` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `group_permissions`
+--
+
+LOCK TABLES `group_permissions` WRITE;
+/*!40000 ALTER TABLE `group_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `group_permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `groups` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,'abba'),(2,'acdc'),(3,'foje');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permissions`
+--
+
+DROP TABLE IF EXISTS `permissions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `permissions` (
+  `permission_id` int(11) NOT NULL AUTO_INCREMENT,
+  `permission_descr` varchar(50) NOT NULL,
+  PRIMARY KEY (`permission_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permissions`
+--
+
+LOCK TABLES `permissions` WRITE;
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `receivers`
+--
+
+DROP TABLE IF EXISTS `receivers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `receivers` (
+  `receiver_id` int(11) NOT NULL AUTO_INCREMENT,
+  `receiv_user_id` int(11) DEFAULT NULL,
+  `receiv_group_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`receiver_id`),
+  KEY `receiv_user_idx` (`receiv_user_id`),
+  KEY `receiv_group_idx` (`receiv_group_id`),
+  CONSTRAINT `receiv_group` FOREIGN KEY (`receiv_group_id`) REFERENCES `groups` (`group_id`),
+  CONSTRAINT `receiv_user` FOREIGN KEY (`receiv_user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `receivers`
+--
+
+LOCK TABLES `receivers` WRITE;
+/*!40000 ALTER TABLE `receivers` DISABLE KEYS */;
+INSERT INTO `receivers` VALUES (1,2,NULL),(2,NULL,3),(3,NULL,1),(4,1,NULL),(5,NULL,2);
+/*!40000 ALTER TABLE `receivers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `status`
+--
+
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `status` (
+  `status_id` int(11) NOT NULL AUTO_INCREMENT,
+  `status_descr` varchar(50) NOT NULL,
+  PRIMARY KEY (`status_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `status`
+--
+
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'sukurtas'),(2,'pateiktas'),(3,'priimtas'),(4,'atmestas');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_groups`
+--
+
+DROP TABLE IF EXISTS `user_groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `user_groups` (
+  `user_group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_group_id`),
+  KEY `group_FK_idx` (`group_id`),
+  KEY `user_groups_user_FK_idx` (`user_id`),
+  CONSTRAINT `user_groups_group_FK` FOREIGN KEY (`group_id`) REFERENCES `groups` (`group_id`),
+  CONSTRAINT `user_groups_user_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_groups`
+--
+
+LOCK TABLES `user_groups` WRITE;
+/*!40000 ALTER TABLE `user_groups` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `surname` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `position` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Test-user4','marius-test','wow','best@emails.com','driver'),(2,'boi','bestboi','asd','izi@email.com','pointer');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-03-13  9:45:24
